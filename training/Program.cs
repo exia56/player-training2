06c9944ea32a6e5e3efc4f2c7f9bde658fe9dbac
Mike <>
2019-01-13 19:58:43 +0800

    hello world

---
+++ training/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace training
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("初始化程式～");
+      Console.WriteLine("選擇播放的影片");
+      Console.WriteLine("使用播放器1來播放影片");
+      Console.WriteLine("播放器1開始播放影片");
+      Console.WriteLine("播放中～～～");
+      Console.WriteLine("播放完畢～釋放資源");
+    }
+  }
+}
